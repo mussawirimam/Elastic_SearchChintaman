23467a1ce5243490758a11f168c82f6365c31d89
Chintaman <chintaman@sarithm.com>
2025-08-21 10:44:18 +0530

    d

--- Test.py
+++ Test.py
@@ -1,7 +1,19 @@
-{ 
+import json
+
+mydocs= { 
     "Students":    {    
     "Bob":{"sername":"{sername:rathore,age:31,\"under19:False,speedofiq:1.3}","age":30,"under19":"True","speedofiq":1.2},
-    "mic": {"sername":"rathore","age":31,"under19":False,"speedofiq":1.3},
+    "mic": {"sername":"rathore","age":31,"under19":"False","speedofiq":1.3},
     "quick": {"sername":["romey","tommy"],"age":30,"under19":"True","speedofiq":1.2}
     }
 }
+
+student = mydocs.get("Students")      
+
+print(student.get("Bob"))
+
+
+
+
+
+
